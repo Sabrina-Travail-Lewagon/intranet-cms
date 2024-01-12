c2c67b8ef26fafb573e30fc347132f20f42c0f39
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2024-01-12 13:43:07 +0100

    ajout de la partie modification d'une catégorie : OK

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
   def index
+
   end
 
   def show
   end
 
   def new
+
   end
 
   def edit
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,20 +8,29 @@ class CategoriesController < ApplicationController
   end
 
   def new
-    @user = current_user
     @category = Category.new
   end
 
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to  categories_path(@category), notice: 'Categorie créée!'
+      redirect_to  category_path(@category), notice: 'Categorie créée!'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "Catégorie a été mis à jour."
+    else
+      render :edit, alert: "Impossible de mettre à jour la catégorie."
+    end
   end
 
   private
