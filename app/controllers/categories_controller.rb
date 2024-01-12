ed7e9e3ea8603cce651a62b47e251fb96b17f4fc
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2024-01-12 12:12:49 +0100

    install active_storage, creation partie CRUD categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,31 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def new
+    @user = current_user
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to  categories_path(@category), notice: 'Categorie créée!'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:nom, :image, :background_color)
   end
 end
